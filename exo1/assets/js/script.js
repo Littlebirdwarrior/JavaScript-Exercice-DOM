f13c8f6b236ea8bb361a6b1c0d8e817afb034dcd
Littlebirdwarrior <camille.dorland@gmail.com>
2023-02-23 15:42:44 +0100

    function shuffle et append Child ok

--- exo1/assets/js/script.js
+++ exo1/assets/js/script.js
@@ -1,3 +1,37 @@
 //Script
 
-console.log('helloWorld');
+
+//mélanger les cartes
+function shuffleChildren(parent){
+    let children = parent.children
+    let i = children.lenght, k, temp
+    while(--i > 0){
+        k = Math.floor(Math.random()*(i+1))
+        temp = board.childNodes[k]
+        board.childNodes[k] = board.childNodes[i]
+        board.appendChild(temp)
+    }
+}
+
+//generer les cartes
+const box = document.createElement('div')
+box.classList.add('box')
+
+//selectionnner contenant
+const board = document.querySelector('#board')
+
+
+
+//génerer cartes et les numeroté
+for(let i = 1; i <= 10; i++){
+    const newbox = box.cloneNode()
+    newbox.innerText = i
+    board.appendChild(newbox)
+
+    newbox.addEventListener('click', function(){
+    console.log("Boite n°"+ i +", click !");
+    })
+}
+
+    
+shuffleChildren(board)
